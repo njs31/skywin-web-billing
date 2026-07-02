34103830145fd579f517929e74a204ecdfbc133b
Jai Santosh <jaisantosh31@gmail.com>
2026-07-02 15:28:39 +0530

    changes

--- app/customers/[id]/page.tsx
+++ app/customers/[id]/page.tsx
@@ -4,6 +4,7 @@ import {
   getCustomerById,
   getCustomerOutstanding,
   getCustomerSales,
+} from "@/lib/queries/customers";
 import { formatCurrency, formatDateIST } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
